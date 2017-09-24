6a87111edc4459e600269ed0aa450c6fbfb33c76
Hardit Singh <hardit@outlook.com>
2017-09-24 19:59:49 +0200

    Forgot the Main class :'(

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,36 +25,7 @@ public class Main {
 		consoleInput.close();
 	}
 	
-	/*private void actualStart() {
-		Scanner consoleInput = new Scanner(System.in);
-		consoleInput.useDelimiter(" |\n|\r|\n\r");
-
-		while (consoleInput.hasNext()) {
-			String line = consoleInput.next();
-			
-			if(Character.isAlphabetic(line.charAt(0))) {
-				String name = consoleInput.next();
-				consoleInput.useDelimiter("");
-				consoleInput.next();
-				consoleInput.useDelimiter(" |\n|\r|\n\r");
-				String data = consoleInput.nextLine();
-				resources.setStore(name, resources.processEBNF(data));
-			}
-		}
-	}
-
-	void test() {
-		Scanner consoleInput = new Scanner(System.in);
-		resources.processInput(consoleInput.nextLine());
-//		SetScanner trial = new SetScanner(consoleInput.nextLine());
-//		System.out.println(trial.nextString());
-//		System.out.println(trial.returnNextType());
-//		System.out.println(trial.nextString());
-//		System.out.println(trial.returnNextType());
-//		System.out.println(trial.nextSet());
-		
-	}
-	*/
+	
 	public static void main(String[] argv) {
 		new Main().start();
 	}
